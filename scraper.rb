98bd6b00b5eefdcb2701073b43bf9b82b6285f65
Adrian McEwen <adrianm@mcqn.com>
2020-02-15 21:48:41 +0000

    Save both decided and validated applications to the database.

--- scraper.rb
+++ scraper.rb
@@ -6,9 +6,14 @@ auths = UKPlanningScraper::Authority.tagged('liverpoolcityregion')
 auths.each_with_index do |auth, i|
   begin
     puts "#{i + 1} of #{auths.size}: Scraping #{auth.name}"
+    puts "  Checking for validated applications..."
+    apps = auth.scrape({ validated_days: ENV['MORPH_DAYS'].to_i })
+    ScraperWiki.save_sqlite([:authority_name, :council_reference], apps)
+    puts "  #{auth.name}: #{apps.size} application(s) saved."
+    puts "  Now getting decided applications..."
     apps = auth.scrape({ decided_days: ENV['MORPH_DAYS'].to_i })
     ScraperWiki.save_sqlite([:authority_name, :council_reference], apps)
-    puts "#{auth.name}: #{apps.size} application(s) saved."
+    puts "  #{auth.name}: #{apps.size} application(s) saved."
   rescue StandardError => e
     puts e
   end
